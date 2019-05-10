ff6e00f93fc4d81ad1a14825dcb2ff7ed491946f
Worker-Heaven <superstar.geekboy@gmail.com>
2019-05-10 14:41:39 +0800

    Consider csrf token

--- mainboard/views.py
+++ mainboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 
 from mainboard.models import Site, Item
@@ -7,20 +8,25 @@ from mainboard.models import Site, Item
 def index(request):
     template = loader.get_template('mainboard/index.html')
 
+    sites = Site.objects.all()
     context = {
-
+        'sites': sites,
     }
 
     return HttpResponse(template.render(context, request))
 
 
 def add(request):
-    template = loader.get_template('mainboard/add.html')
+    if request.method == 'GET':
+        template = loader.get_template('mainboard/add.html')
 
-    items = Item.objects.all()
+        items = Item.objects.all()
 
-    context = {
-        'items': items,
-    }
+        context = {
+            'items': items,
+        }
 
-    return HttpResponse(template.render(context, request))
+        return HttpResponse(template.render(context, request))
+
+    else:
+        return HttpResponseRedirect(reverse('index', args=()))
